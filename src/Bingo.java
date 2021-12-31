dfa816656c9c5165aa38f986d1cefb7246e87693
Patrick Celedio <patrickceledio@gmail.com>
2021-12-30 20:51:51 -0800

    Updated

--- src/Bingo.java
+++ src/Bingo.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Bingo {
     static Scanner sc = new Scanner(System.in);
     static Random rand = new Random();
-    static void menuPrompt(){
+
+    static void menuPrompt() {
         System.out.println("Welcome to Bingo\nPlease select an option.\nOptions:\n1. Play\n2. Rules\n3. Credits");
         int choice = sc.nextInt();
-        switch(choice){
+        switch (choice) {
             case 1:
                 play();
                 menuPrompt();
@@ -26,13 +27,13 @@ public class Bingo {
         }
     }
 
-    static String drawCombo(){
+    static String drawCombo() {
         String bingo = "BINGO";
         char resultLetter = bingo.charAt(rand.nextInt(5));
 
         int low = 1;
         int high = 50;
-        int resultNum = rand.nextInt(high-low) + low;
+        int resultNum = rand.nextInt(high - low) + low;
 
         String chance = resultLetter + String.valueOf(resultNum);
         System.out.println("Computer drew: " + chance);
@@ -40,18 +41,18 @@ public class Bingo {
         return chance;
     }
 
-    static String[] drawBingoCard(){
+    static String[] drawBingoCard() {
         String bingo = "BINGO";
         char resultLetter = bingo.charAt(rand.nextInt(5));
 
         int low = 1;
         int high = 50;
-        int resultNum = rand.nextInt(high-low) + low;
+        int resultNum = rand.nextInt(high - low) + low;
 
         String[] bingoCard = new String[20];
 
-        for (int i = 0; i<20; i++){
-            Integer number = rand.nextInt((high-low) + low );
+        for (int i = 0; i < 20; i++) {
+            Integer number = rand.nextInt((high - low) + low);
             char letter = bingo.charAt(rand.nextInt(5));
             String combo = letter + String.valueOf(number);
             bingoCard[i] = combo;
@@ -60,56 +61,80 @@ public class Bingo {
         return bingoCard;
     }
 
-    static void printBingoCard(String[] bingoCard){
+    static void printBingoCard(String[] bingoCard) {
         System.out.println();
         System.out.println("| \tB\t | \tI\t | \tN\t | \tG\t | \tO\t |\t");
-        System.out.println("| \t" + bingoCard[0] + "\t | \t" + bingoCard[1]  + "\t | \t"
-                + bingoCard[2] + " | \t" + bingoCard[3]  + " | \t" +  bingoCard[4] + " |\t");
-        System.out.println("| \t" + bingoCard[5] + "\t | \t" + bingoCard[6]  + "\t | \t"
-                + bingoCard[7] + " | \t" + bingoCard[8]  + " | \t" +  bingoCard[9] + " |\t");
-        System.out.println("| \t" + bingoCard[10] + "\t | \t" + bingoCard[11]  + "\t | \t"
-                + bingoCard[12] + " | \t" + bingoCard[13]  + " | \t" +  bingoCard[14] + " |\t");
-        System.out.println("| \t" + bingoCard[15] + "\t | \t" + bingoCard[16]  + "\t | \t"
-                + bingoCard[17] + " | \t" + bingoCard[18]  + " | \t" +  bingoCard[19] + "\t |\t");
+        System.out.println("-------------------------------------------");
+        System.out.println("| \t" + bingoCard[0] + "\t | \t"
+                + bingoCard[1] + "\t | \t"
+                + bingoCard[2] + "\t | \t"
+                + bingoCard[3] + "\t | \t"
+                + bingoCard[4] + "\t |\t");
+        System.out.println("-------------------------------------------");
+        System.out.println("| \t" + bingoCard[5] + "\t | \t"
+                + bingoCard[6] + "\t | \t"
+                + bingoCard[7] + "\t | \t"
+                + bingoCard[8] + "\t | \t"
+                + bingoCard[9] + "\t |\t");
+        System.out.println("-------------------------------------------");
+        System.out.println("| \t" + bingoCard[10] + "\t | \t"
+                + bingoCard[11] + "\t | \t"
+                + bingoCard[12] + "\t | \t"
+                + bingoCard[13] + "\t | \t"
+                + bingoCard[14] + "\t |\t");
+        System.out.println("-------------------------------------------");
+        System.out.println("| \t" + bingoCard[15] + "\t | \t"
+                + bingoCard[16] + "\t | \t"
+                + bingoCard[17] + "\t | \t"
+                + bingoCard[18] + "\t | \t"
+                + bingoCard[19] + "\t |\t");
     }
 
-    static void play(){
+    static void play() {
         clearConsole();
         System.out.println("Play");
 
-        // Generate and save bingo card and chance
+        // Generate and save bingo card
         final String[] userBingoCard = drawBingoCard();
+
+        // Generate and save bingo number
         String drawnChance = drawCombo();
+
         printBingoCard(userBingoCard);
 
+//        while(true){
+//            if ()
+//        }
 
     }
 
-    static void rules(){
+    static void rules() {
         clearConsole();
         System.out.println("Rules");
     }
 
-    static void credits(){
+    static void credits() {
         clearConsole();
         System.out.println("Credits");
     }
 
-    static void clearConsole(){
-        try{
+    static void clearConsole() {
+        try {
             final String os = System.getProperty("os.name");
 
-            if (os.contains("Windows")){
+            if (os.contains("Windows")) {
                 Runtime.getRuntime().exec("cls");
-            }else{
+            } else {
                 Runtime.getRuntime().exec("clear");
             }
 
-        }catch (final Exception e){
+        } catch (final Exception e) {
             System.out.println("Could not clear console.\n");
         }
     }
+
     public static void main(String[] args) {
+        System.out.println("Hello world!");
         menuPrompt();
     }
 }
